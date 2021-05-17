cf98ef1771a96949dbcf7b1d13f36aa4d4fc3c50
mac <shumnox@gmail.com>
2021-05-17 18:47:33 +0300

    homework completely

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -10,11 +10,39 @@ function getResult(a,b,c){
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+    const marksArray = [...marks];
+    marksArray.forEach((item) => +item);
+    let averageMark = [];
+
+    if (marksArray.length === 0) {
+        averageMark = 0;
+    } else if (marksArray.length > 5) {
+        console.log("Слишком много оценок, будем считать только первые 5!");
+        const slicedMarksArray = marksArray.slice(0,5);
+        let sum = slicedMarksArray.reduce(function(sum, elem) {
+            return sum + elem;
+        }, 0);
+        averageMark = sum / slicedMarksArray.length;
+    } else {
+        let sum = marksArray.reduce(function(sum, elem) {
+            return sum + elem;
+        }, 0);
+        averageMark = sum / marksArray.length;
+    }
+    return averageMark;
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+    let today = new Date();
+    let year = today.getFullYear();
+    let birthdayYear = dateOfBirthday.getFullYear();
+    let age = year - birthdayYear;
+    let result;
+    if (age >= 18) {
+        result = ("Не желаете ли олд-фэшн, " + name + "?");
+    } else {
+        result = ("Сожалею, " + name + ", " + "но я не могу вам продать алкоголь. " +
+            "Могу предложить вам замечательный клюквенный компот!")
+    };
+    return result;
 }
