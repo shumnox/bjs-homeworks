d75db406ce489f81c0e1b2e91a772cf38c525a4b
shumnox <shumnox@gmail.com>
2021-05-24 18:01:26 +0300

    homework completely

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,6 +1,33 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    "use strict";
+    let percentVal = parseInt(percent) / 100;
+    if (Number.isNaN(percent)) {
+       return "Параметр percent содержит неправильное значение" + percentVal;
+    }
+    // сумму первоначального взноса
+    let contributionVal = parseInt(contribution);
+    if (Number.isNaN(contribution)) {
+        return "Параметр percent содержит неправильное значение" + contributionVal;
+    }
+    // сумму кредита
+    let amountVal = parseInt(amount);
+    if (Number.isNaN(amount)) {
+        return "Параметр percent содержит неправильное значение" + amountVal;
+    }
+
+    let today = new Date();
+    let month  = (date.getFullYear() - today.getFullYear()) * 12;
+    month  -= today.getMonth();
+    month  += today.getMonth();
+
+    // тело кредита
+    let bankAmount = amountVal - contributionVal;
+    let monthPercent = percentVal / 12;
+
+    let monthAmount = bankAmount * (monthPercent + monthPercent / (((1 + monthPercent)^month) - 1));
+    let totalAmount = monthAmount * month;
+    totalAmount = ( Math.round(totalAmount * 100) / 100 );
+    return totalAmount;
 }
 
 function getGreeting(name) {
